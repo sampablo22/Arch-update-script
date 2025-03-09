2f075c5dc4664faa4dfcdf2cc9dfe480cf2e1731
Sam McMaster <sam.mcmaster79@gmail.com>
2025-03-09 17:50:58 +0000

    Added additional commands

--- update_Arch.sh
+++ update_Arch.sh
@@ -4,8 +4,37 @@ echo "Hello "$USER", your operating system and repositories will now be updated.
 while true; do
     read -p "Do you wish to run the update command? " yn
     case $yn in
-        [Yy]* ) sudo pacman -Sy; echo "Thanks for updating me."; break;;
+        [Yy]* ) sudo pacman -Syu; echo "Thanks for updating me."; break;;
         [Nn]* ) break;;
         * ) echo "Please answer yes or no.";;
     esac
 done
+
+while true; do
+    read -p "Do you want to force a database refresh? " yn
+    case $yn in
+        [Yy]* ) sudo pacman -Syyu; echo "Thanks for refreshing the database."; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+
+while true; do
+    read -p "Do you want to clean the package cache? " yn
+    case $yn in
+        [Yy]* ) sudo pacman -Sc; echo "Thanks for cleaning the package cache."; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+while true; do
+    read -p "Do you want to remove orphaned packages? " yn
+    case $yn in
+        [Yy]* ) sudo pacman -Rns $(pacman -Qtdq); echo "Thanks for removing orphaned packages."; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+echo "All done. Have a great day!"
+exit 0
